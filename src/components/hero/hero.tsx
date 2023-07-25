dda9caf000509bef2b4020d4fee6985e9957a333
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-07-25 22:53:29 +0500

    hero

---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,7 @@
+import { IHeroProps } from './hero.props'
+
+const Hero = ({ trending }: IHeroProps): JSX.Element => {
+  return <div> Hero</div>
+}
+
+export default Hero
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as Header } from './header/Header'
 export { default as Row } from './row/Row'
+export { default as Hero } from './hero/hero'
