da6d636a285c5227ff5c91fa93d1dece59c27c0d
Jonibek <94430009+MJony05@users.noreply.github.com>
2023-07-26 19:18:56 +0500

    formik validation

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as Header } from './header/Header'
 export { default as Row } from './row/Row'
 export { default as Hero } from './hero/hero'
+export { default as TextField } from './text-field/text-field'
